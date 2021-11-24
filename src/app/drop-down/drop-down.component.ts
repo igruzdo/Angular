91b7b59ef2ba634e7de7b3eb3b9fd2c5b9ac020c
Sasha <ashatilovdev@gmail.com>
2021-11-25 00:29:20 +0300

    поправил работу меню

--- src/app/drop-down/drop-down.component.ts
+++ src/app/drop-down/drop-down.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-drop-down',
   template: `
     <div class="dropdown">
-      <app-button color="default" size="default" (click)="isShowToggleClick()" (mouseover)="isShowToggleHover()"></app-button>
+      <app-button color="default" size="default" (click)="isShowToggleClick()" ></app-button>
       <div *ngIf='isShow' class="menu-wrapper">
         <app-menu ></app-menu>
       </div>
@@ -14,6 +14,7 @@ import {Component, Input, OnInit} from '@angular/core';
     `
     .dropdown {
       position: relative;
+      display: inline-block;
     }
     .menu-wrapper {
       position: absolute;
@@ -40,6 +41,18 @@ export class DropDownComponent implements OnInit {
   isShowToggleHover() {
     if(this.trigger === 'hover'){
       this.isShow = !this.isShow;
+    }
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    if (this.trigger === 'hover') {
+      this.isShow = true;
+    }
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    if (this.trigger === 'hover') {
+      this.isShow = false;
     }
   }
 }
