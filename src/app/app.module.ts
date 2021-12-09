d852b14123a73bf811329b2e9f39781f788d96f1
Sasha <ashatilovdev@gmail.com>
2021-12-09 21:21:26 +0300

    убрал каталог модуля из app модуля, иначе не грузился лениво

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {DropDownModule} from "./drop-down/drop-down.module";
-import {CatalogModule} from "./catalog/catalog.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {NotFoundModule} from "./not-found/not-found.module";
 
@@ -14,7 +13,6 @@ import {NotFoundModule} from "./not-found/not-found.module";
   imports: [
     BrowserModule,
     DropDownModule,
-    CatalogModule,
     AppRoutingModule,
     NotFoundModule
   ],
